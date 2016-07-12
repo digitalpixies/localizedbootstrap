eb60f3ad8abea76fb2b9229eaf1347eb82c7ac52
Robert Huie <rhuie@digitalpixies.com>
2016-07-12 14:30:41 -0700

    Squashed commit of the following:
    commit c4899ee2abe8c3e10f05936df01a6813e74751f0
    Author: Robert Huie <rhuie@digitalpixies.com>
    Date:   Tue Jul 12 13:52:31 2016 -0700
    
        3.0.3 to 3.3.6

--- dist/css/make.sh
+++ dist/css/make.sh
@@ -1,2 +1,5 @@
+cp bootstrap.css bootstrap.css.less
 lessc localizedbootstrap.less localizedbootstrap.css
+cp bootstrap-theme.css bootstrap-theme.css.less
 lessc localizedbootstrap-theme.less localizedbootstrap-theme.css
+
