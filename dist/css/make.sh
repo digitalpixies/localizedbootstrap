48636288d72a8740e72f80ff44a1448b725e98e5
Robert MBP Huie <rhuie@digitalpixies.com>
2017-05-26 16:04:05 -0700

    Update to 3.3.7
    * compile using lessc from npm

--- dist/css/make.sh
+++ dist/css/make.sh
@@ -1,5 +1,5 @@
 cp bootstrap.css bootstrap.css.less
-lessc localizedbootstrap.less localizedbootstrap.css
+../../node_modules/less/bin/lessc localizedbootstrap.less localizedbootstrap.css
 cp bootstrap-theme.css bootstrap-theme.css.less
-lessc localizedbootstrap-theme.less localizedbootstrap-theme.css
+../../node_modules/less/bin/lessc localizedbootstrap-theme.less localizedbootstrap-theme.css
 
